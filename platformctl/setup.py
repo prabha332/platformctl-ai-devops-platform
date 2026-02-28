e8ba42400e3ffd109b809e48980969ff6c52250b
prabha332 <prabhavatiagre@gmail.com>
2026-02-28 20:13:18 +0530

    Add setup.py for platformctl package

--- platformctl/setup.py
+++ platformctl/setup.py
@@ -1,1 +1,15 @@
+from setuptools import setup, find_packages
 
+setup(
+    name="platformctl",
+    version="1.0",
+    packages=find_packages(),
+    install_requires=[
+        "typer[all]"
+    ],
+    entry_points={
+        "console_scripts": [
+            "platformctl=cli.main:app"
+        ]
+    },
+)
