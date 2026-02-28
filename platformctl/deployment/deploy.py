b60c3ed0ac4005cd73ca53103ed259e34df9a27f
prabha332 <prabhavatiagre@gmail.com>
2026-02-28 10:03:01 +0530

    Add deploy_app function for Kubernetes deployment

--- platformctl/deployment/deploy.py
+++ platformctl/deployment/deploy.py
@@ -1,1 +1,11 @@
+import subprocess
 
+def deploy_app(file):
+    print("Deploying application...")
+
+    subprocess.run(
+        ["kubectl", "apply", "-f", file],
+        check=True
+    )
+
+    print("Deployment successful")
